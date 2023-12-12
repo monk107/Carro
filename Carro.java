d4eac194046673979f4d04aecf97fc7a15bfb0b1
monk107 <83814865+monk107@users.noreply.github.com>
2023-12-12 10:08:51 +0100

    Add files via upload

---
+++ Carro.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Carro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       ArrayList<String> carro= new ArrayList<String>();
+       carro.add("maduixes");
+       carro.add("pa");
+       
+       
+       
+       System.out.println(carro);
+       
+       for(String item: carro) {
+    	   System.out.println(item);
+       }
+      
+       
+       
+       
+	}
+
+}
